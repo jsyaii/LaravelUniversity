bb0953816cd0ce150b6268e1a36faae2beab7128
jsyaii <jasiahasan1703@gmail.com>
2024-12-05 18:41:19 +0600

    first commit

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -130,4 +130,4 @@
 
 </body>
 
-</html>
+</html> -->
--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,9 @@ Route::get('/home', function () {
 
 Route::get('/search', [Search::class, "showRecords"]);
 Route::post('/search', [Search::class, 'searchRecords'])->name('search.records');
-Route::get('/manage', [Users::class, "show_users"]);
-Route::post('/manage/editName/{id}', [Users::class, "changeName2"]);
-Route::get('/manage/deleteRecord/{id}', [Users::class, "deleteRecord"]);
-Route::get('manage/showForm', [Users::class, "show_form"]);
-Route::get('/manage/editName/{id}', [Users::class, "show_edit_form"]);
-Route::post('/manage/create', [Users::class, "createRecord"]);
+// Route::get('/manage', [Users::class, "show_users"]);
+// Route::post('/manage/editName/{id}', [Users::class, "changeName2"]);
+// Route::get('/manage/deleteRecord/{id}', [Users::class, "deleteRecord"]);
+// Route::get('manage/showForm', [Users::class, "show_form"]);
+// Route::get('/manage/editName/{id}', [Users::class, "show_edit_form"]);
+// Route::post('/manage/create', [Users::class, "createRecord"]);
